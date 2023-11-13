7560095656207dbfcd32c9949510a80cc086e8bd
anil_96 <anil@vishalvishwakarma.com>
2023-11-13 11:33:59 +0530

    'added'

--- resources/views/backend/vendor/payment.blade.php
+++ resources/views/backend/vendor/payment.blade.php
@@ -3,7 +3,7 @@
 @include('backend.includes.datatablesCSS')
 @endsection
 @section('contentHeader')
-Branches
+Vendor Payment
 @endsection
 @section('contentButton')
 <a class="btn btn-success btn-sm modal-trigger" data-toggle="modal" data-target="#modal-add">Add New Payment</a>
